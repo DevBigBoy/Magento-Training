efd2b29058e630364e6484328772779e0f975530
BigBoy <moshazly416@gmail.com>
2025-07-13 10:00:00 +0300

    How to create crons in Magento 2

--- app/code/Training/CronExample/Cron/Example.php
+++ app/code/Training/CronExample/Cron/Example.php
@@ -2,7 +2,34 @@
 
 namespace Training\CronExample\Cron;
 
+use Psr\Log\LoggerInterface;
+
 class Example
 {
 
+    /**
+     * @var LoggerInterface
+     */
+    protected LoggerInterface $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        LoggerInterface $logger
+    ) {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Write to system.log
+     *
+     * @return void
+     */
+    public function execute(): void
+    {
+        $this->logger->info('CronExample: Cron Works');
+        sleep(3);
+        $this->logger->info('CronExample: Cron Finished');
+    }
 }
