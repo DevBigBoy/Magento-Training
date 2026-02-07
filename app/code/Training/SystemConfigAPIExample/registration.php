5c77c37cf7a54cdb1fbdd4e837818874324a2e56
BigBoy <moshazly416@gmail.com>
2026-02-07 16:48:01 +0200

    How to add system config with ACL and API example in Magento

--- app/code/Training/SystemConfigAPIExample/registration.php
+++ app/code/Training/SystemConfigAPIExample/registration.php
@@ -6,4 +6,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Training_SystemConfigAPIExample', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Training_SystemConfigExample', __DIR__);
